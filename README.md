7101e4437fd650bf1beca88c1de2e8d4104ac96a
Nathan Friedly <nathan@nfriedly.com>
2015-05-08 10:25:44 -0400

    setup instructions and further simplification

--- README.md
+++ README.md
@@ -3,4 +3,10 @@ Streaming Watson Text to Speech player
 
 Node.js script that plays synthesized voice audio over your computer's speakers.
 
-Currently requires Linux or OS X, Windows support may come in the future.
+Currently requires Linux or OS X. Windows support may come in the future.
+
+
+Setup
+-----
+
+Create a watson Text to Speech instance in [Bluemix](http://bluemix.net/), bind it to a server, view the server's dashboard, and click "View Credentials" on your TTS service. Copy the username and password to `config.js`
--- player.js
+++ player.js
@@ -1,4 +1,3 @@
-var fs = require('fs');
 var watson = require('watson-developer-cloud');
 var ogg = require('ogg');
 var opus = require('node-opus');
@@ -13,10 +12,11 @@ var text_to_speech = watson.text_to_speech({
 
 var params = {
     text: 'Hello from IBM Watson',
-    accept: 'audio/ogg; codec=opus' //  audio/wav or audio/ogg; codec=opus
+    accept: 'audio/ogg; codec=opus'
 };
 
-// ogg.Decoder detects the opus audio stream embedded in the container and emits a "stream" event with it
+// text_to_speech.synthesize returns a stream of ogg/opus data
+// ogg.Decoder detects the opus audio sub-stream embedded in the ogg container and emits a "stream" event with it
 // opus.Decoder decodes this stream to PCM data, and also emits a "format" event with frequency, # chanels, etc.
 // Speaker uses the format and PCM data to play audio on your system's speakers
 
@@ -27,24 +27,3 @@ text_to_speech.synthesize(params)
             .pipe(new Speaker());
     });
 
-
-/*
-$ opusinfo test.opus
-Processing file "test.opus"...
-
-New logical stream (#1, serial: c3ebd6b6): type opus
-Encoded with Lavf56.18.100
-User comments section follows...
-	encoder=Lavc56.20.100 libopus
-Opus stream 1:
-	Pre-skip: 156
-	Playback gain: 0 dB
-	Channels: 1
-	Original sample rate: 24000Hz
-	Packet duration:   20.0ms (max),   20.0ms (avg),   20.0ms (min)
-	Page duration:   1000.0ms (max),  693.3ms (avg),   80.0ms (min)
-	Total data length: 18741 bytes (overhead: 1.77%)
-	Playback length: 0m:02.069s
-	Average bitrate: 72.45 kb/s, w/o overhead: 71.17 kb/s
-Logical stream 1 ended
- */
